277a5a453c151edc481972e25706c7211a0f9373
Sam K Varghese <ksam071@gmail.com>
2022-12-31 17:36:10 +0530

    Program to Swap Two Numbers

---
+++ src/Swap.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Swap {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c=0, d=0;
+        System.out.print("Enter the first Number :");
+        a = sc.nextInt();
+        System.out.print("Enter the second Number :");
+        b = sc.nextInt();
+        System.out.print("The numbers are A = " + a + "  and B = " + b);
+        d = a;
+        c = b;
+        System.out.println("\n");
+        System.out.println("After swapping");
+        System.out.println("\n");
+        System.out.print("The numbers are A = " + c + "  and B = " + d);
+    }
+}
+
